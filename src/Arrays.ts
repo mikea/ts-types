c13da0df011693cf0e2450809eecde74f8fbbba3
Mike Aizatsky <mike.aizatsky@gmail.com>
2022-03-30 10:04:05 -0700

    array1 & array2

--- src/Arrays.ts
+++ src/Arrays.ts
@@ -3,4 +3,11 @@ import { Extends, Not } from "./Predicates";
 export type IsArray<T> = Extends<T, any[]>;
 export type NotArray<T> = Not<IsArray<T>>;
 
+// extract element type from the array
 export type ElementType<T> = T extends Array<infer E> ? E : never;
+
+// array with at lest 1 member
+export type Array1<T> = [T, ...Array<T>];
+
+// array with at lest 2 member
+export type Array2<T> = [T, T, ...Array<T>];
